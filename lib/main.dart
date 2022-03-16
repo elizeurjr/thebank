5abf13e2d2c7e456b802ee535de524a9a93ccea9
zeurjr <elizeuribeiroo@gmail.com>
2022-03-15 21:38:25 -0300

    Extração da Lista de transferencia

--- lib/main.dart
+++ lib/main.dart
@@ -12,13 +12,7 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('The Bank'),
         ),
-        body: Card(
-          child: ListTile(
-            leading: Icon(Icons.monetization_on),
-            title: Text('titulo transferencia'),
-            subtitle: Text('valor transferencia'),
-          ),
-        ),
+        body: ListaTransferencia(),
         floatingActionButton: FloatingActionButton(
           child: Icon(Icons.add),
           onPressed: () {},
@@ -26,4 +20,37 @@ class MyApp extends StatelessWidget {
       ),
     );
   }
+}
+
+class ListaTransferencia extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ItemTransferencia('100', '1000'),
+        ItemTransferencia('110', '1100'),
+      ],
+    );
+  }
+}
+
+class ItemTransferencia extends StatelessWidget {
+
+  final String numeroConta;
+  final String valor;
+
+  ItemTransferencia(this.numeroConta, this.valor);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text(numeroConta),
+        subtitle: Text(valor),
+      ),
+    );
+  }
+
 }
